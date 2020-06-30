a54f049b7dc758c5e16c9278d481534165408d2a
letieu <letieu8@gmail.com>
2020-06-30 20:54:47 +0700

    xong gui va nhan tin

--- addons_social/social/models/mail_channel.py
+++ addons_social/social/models/mail_channel.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api
-
+import requests
+import logging
+import json
 
 class MailChannelInherit(models.Model):
 
@@ -11,5 +13,28 @@ class MailChannelInherit(models.Model):
 
     def message_post(self, message_type='notification', **kwargs):
         message = super(MailChannelInherit, self).message_post(message_type='notification', **kwargs)
+
+        if self.facebook_PSID and not kwargs.get("webhook"):
+            message_obj = {
+                "messaging_type": "RESPONSE",
+                "recipient": {
+                    "id": self.facebook_PSID
+                },
+                "message": {
+                    "text": kwargs.get('body')
+                }
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+            access_token = self.social_account_id.facebook_access_token
+            url = "https://graph.facebook.com/v6.0/me/messages?access_token={access_token}".format(
+                access_token=access_token)
+
+            try:
+                response = requests.post(url, headers=headers, data=json.dumps(message_obj))
+            except Exception as e:
+
+                logging.error("Khong gui duoc tin nhan")
 
         return message
